17e9f0317fcb0b972c6bb6bc601ea37056ab95f0
SoundhararajanL <sowndhar00001@gmail.com>
2024-05-13 12:43:51 +0530

    navbar

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import 'font-awesome/css/font-awesome.min.css';
-import './navbar.css';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
+import "./navbar.css";
 
 const Navbar = () => {
   const [isResponsive, setIsResponsive] = useState(false);
@@ -11,23 +11,29 @@ const Navbar = () => {
   };
 
   return (
-    <div className={`topnav ${isResponsive ? 'responsive' : ''}`} id="myTopnav">
-      
+    <div className={`topnav ${isResponsive ? "responsive" : ""}`} id="myTopnav">
       <span className="mah"></span>
-    
-      <NavLink className='nav-text' to="/gallery">Gallery🎴</NavLink>
-      <NavLink className='nav-text' to="/book">Book💬</NavLink>
-
-      <NavLink className='nav-text' to="/whatsappstart">Start💕</NavLink>
-      <NavLink className='nav-text' to="/whatsappend">End💔</NavLink>
-      <NavLink className='nav-text' to="/quotes">❝Quotes❞</NavLink>
-
+      <NavLink className="nav-text" to="/whatsappstart">
+        Start💕
+      </NavLink>
+      <NavLink className="nav-text" to="/gallery">
+        Gallery🎴
+      </NavLink>
+      <NavLink className="nav-text" to="/book">
+        Book💬
+      </NavLink>
+      <NavLink className="nav-text" to="/quotes">
+        ❝Quotes❞
+      </NavLink>
+      <NavLink className="nav-text" to="/whatsappend">
+        End💔
+      </NavLink>
 
       <a href="javascript:void(0);" className="icon" onClick={handleToggle}>
         <i className="fa fa-bars"></i>
       </a>
     </div>
   );
-}
+};
 
 export default Navbar;
